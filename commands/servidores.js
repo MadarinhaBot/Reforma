2c7bdc9160a494082bec4c290e86cef6a5fb7dce
Davi2875 <92585553+Davi2875@users.noreply.github.com>
2021-12-17 09:39:28 -0300

    Create servidores.js

---
+++ commands/servidores.js
@@ -0,0 +1,75 @@
+### MUDEM O QUE ESTIVER ERRADO
+
+//Naruto
+
+      coletor.on("collect", c => {
+        let embed = new Discord.MessageEmbed()
+        .setTitle(`Naruto Dark`)
+        .setDescription(`__Informações Sobre o Naruto Dark__:`)
+        .addFields(
+//IP
+
+      value: `play2.narutocdark.com.br`,
+
+//Porta
+
+      value: `19132`,
+
+//Site
+
+      value: `[Clique Aqui](https://pocket.narutocdark.com.br/)`,
+
+//Textura
+
+      value: `[Clique Aqui](https://www.mediafire.com/file/bluh39w5dryz82c/Textura_V5_Naruto_Pocket.mcpack.zip/file)`,
+
+//Download Minecraft
+
+      value: `[Clique Aqui](https://www.mediafire.com/file/cwzzaojhcomb6qz/MCPE_1.17.30_By_DeutschF%25C3%25BChrerGames_%2528Xbox_Live%2529.apk/file)`,
+
+//Dragon Ball
+
+      coletor2.on("collect", c => {
+        let embed = new Discord.MessageEmbed()
+        .setTitle(`Dragon Ball Dark`)
+        .setDescription(`__Informações Sobre o Dragon Ball Dark__:`)
+        .addFields(
+//IP
+
+      value: `dbpe.narutocdark.com.br`,
+
+//Porta
+
+     value: `19062`,
+
+//Site
+      value: `[Clique Aqui](http://pocket.dbdark.com.br/)`,
+
+//Textura
+
+      value: `[Clique Aqui](https://www.mediafire.com/file/orvnnbj3rb50bgf/Dragon+Block+C+dark.zip/file)`,
+
+//Download do Minecraft
+
+      value: `[Clique Aqui](https://www.mediafire.com/file/cwzzaojhcomb6qz/MCPE_1.17.30_By_DeutschF%25C3%25BChrerGames_%2528Xbox_Live%2529.apk/file)`,
+
+//Demon Slayer
+
+        let embed = new Discord.MessageEmbed()
+        .setTitle(`Demon Slayer Dark`)
+        .setDescription(`__Informações Sobre o Demon Slayer Dark__:`)
+        .addFields(
+//IP
+
+      value: `Em bereve`,
+
+//Site
+      value: `Em breve`,
+
+//Textura
+
+      value: `Em Breve`,
+
+//Download Mincraft
+
+      value: `[Clique Aqui](https://www.mediafire.com/file/cwzzaojhcomb6qz/MCPE_1.17.30_By_DeutschF%25C3%25BChrerGames_%2528Xbox_Live%2529.apk/file)`,

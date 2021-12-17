54d06d9ea2f6ada515bdd3092c583523359e0d72
Davi2875 <92585553+Davi2875@users.noreply.github.com>
2021-12-17 09:32:08 -0300

    Create ncdark.js

---
+++ commands/ncdark.js
@@ -0,0 +1,24 @@
+/*
+Mudem o que está errado.
+*/
+
+//IP
+
+      value: `play2.narutocdark.com.br`,
+
+//Porta
+
+      value: `19132`,
+
+//Site
+
+      value: `[Clique Aqui](https://pocket.narutocdark.com.br/)`,
+
+//Textura
+
+      value: `[Clique Aqui](https://www.mediafire.com/file/bluh39w5dryz82c/Textura_V5_Naruto_Pocket.mcpack.zip/file)`,
+
+//Download Minecraft
+
+      value: `[Clique Aqui](https://www.mediafire.com/file/cwzzaojhcomb6qz/MCPE_1.17.30_By_DeutschF%25C3%25BChrerGames_%2528Xbox_Live%2529.apk/file)`,
+

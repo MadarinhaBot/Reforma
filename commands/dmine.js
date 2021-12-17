624308aa49870f5057ac9ee3294648b4064e2055
Rabiurin <81499115+Rabiurin@users.noreply.github.com>
2021-12-17 20:16:59 -0300

    Update dmine.js

--- commands/dmine.js
+++ commands/dmine.js
@@ -3,4 +3,4 @@ Confirmem se o link está certo
 */
 
     .setTitle("Download Minecraft")
-    .setDescription(`\n**Link de Download**:\n\n[Clique Aqui](https://www.mediafire.com/file/cwzzaojhcomb6qz/MCPE_1.17.30_By_DeutschF%25C3%25BChrerGames_%2528Xbox_Live%2529.apk/file).`)
+    .setDescription(`\n**Link de Download**:\n\n[Clique Aqui](https://mega.nz/file/MtkkQRbI#3qjc8wyXs0kLI7Y5TXW9i-xZyzsEuyhtqj-9vQsratE.`)

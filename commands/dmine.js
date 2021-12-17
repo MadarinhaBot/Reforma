aa632df94b1c9cf1732c93afbdbee24a139ed139
Davi2875 <92585553+Davi2875@users.noreply.github.com>
2021-12-17 09:24:31 -0300

    Create dmine.js

---
+++ commands/dmine.js
@@ -0,0 +1,6 @@
+/*
+Confirmem se o link está certo
+*/
+
+    .setTitle("Download Minecraft")
+    .setDescription(`\n**Link de Download**:\n\n[Clique Aqui](https://www.mediafire.com/file/cwzzaojhcomb6qz/MCPE_1.17.30_By_DeutschF%25C3%25BChrerGames_%2528Xbox_Live%2529.apk/file).`)

201ade04cc3ad1a3789d4847fda1ff7d35bf519f
Davi2875 <92585553+Davi2875@users.noreply.github.com>
2021-12-17 09:26:32 -0300

    Create dsdark.js

---
+++ commands/dsdark.js
@@ -0,0 +1,11 @@
+/*
+Tem alguma ideia pra subistituir essa mensagem? Coloque ela como o exemplo abaixo
+
+EX:
+
+//message.madaraReply(`${message.author}, o servidor está quase pronto amigo!`)
+
+OBS: Não apaguem NENHUMA MENSAGEM nesse comando;
+*/
+
+message.madaraReply(`${message.author}, o servidor ainda está sendo feito!`)

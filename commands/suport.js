bd70f1b7c5c8aa85045101b56b3167d4ddaa3bbc
Davi2875 <92585553+Davi2875@users.noreply.github.com>
2021-12-17 09:40:55 -0300

    Create suport.js

---
+++ commands/suport.js
@@ -0,0 +1,9 @@
+/*
+Confirmem se está tudo certo, qualquer dúvida usem o comando em algum chat da DARK (m!suport) e/ou me chamem.
+*/
+      .setTitle(`Duvidas frequentes`)
+      .setColor(`ORANGE`)
+      .addField(`<:MundoMCPE:820728366650621973> | Ip•porta`, `[Clique Aqui!](https://discord.com/channels/716171497835855892/758087262268424204/786567970406662184)`)
+      .addField(`<:mediafire:893980804508696627> | Texturas e Apks:`, `[Clique Aqui!](https://discord.com/channels/716171497835855892/758087260955738142/821034768358440980)`, true)
+      .addField(`<:nitro:893980930589483008> | Vantagens boosters:`, `[Clique Aqui!](https://discord.com/channels/716171497835855892/758087269990269110/776254199682957322)`, true)
+	  .addField(`Ainda precisa de ajuda?`, `Procure por um <@&722980857568165980>!`, true)

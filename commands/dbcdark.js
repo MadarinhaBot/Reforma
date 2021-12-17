79c1a94199afb54ec90d813ab533eb7b917590d5
Davi2875 <92585553+Davi2875@users.noreply.github.com>
2021-12-17 09:23:25 -0300

    Create dbcdark.js

---
+++ commands/dbcdark.js
@@ -0,0 +1,24 @@
+/* 
+
+Confirmem se as informações estão certas, se estiver errado, arrumem pfvr
+
+*/
+
+//IP
+
+      value: `dbpe.narutocdark.com.br`,
+
+//Porta
+
+     value: `19062`,
+
+//Site
+      value: `[Clique Aqui](http://pocket.dbdark.com.br/)`,
+
+//Textura
+
+      value: `[Clique Aqui](https://www.mediafire.com/file/orvnnbj3rb50bgf/Dragon+Block+C+dark.zip/file)`,
+
+//Download do Minecraft
+
+      value: `[Clique Aqui](https://www.mediafire.com/file/cwzzaojhcomb6qz/MCPE_1.17.30_By_DeutschF%25C3%25BChrerGames_%2528Xbox_Live%2529.apk/file)`,
